ad617164e437db393a0b830d7c05fc108cace639
Kevh <k.campi@sapalomer.cat>
2026-01-15 19:53:37 +0100

    Correccion de estructura

---
+++ Practica 2 - Estructures/Program.cs
@@ -0,0 +1,155 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.Serialization.Formatters;
+
+int queTriar;
+Console.Write("Quin es l'exercici que vols triar? ");
+queTriar = Convert.ToInt32(Console.ReadLine());
+switch (queTriar)
+{
+
+    case 1:
+        //Variables
+        int numero = 0;
+        int contador = 13;
+        int numSuma = 13;
+        //Codi
+        Console.Write("Introdueix un numero (mes gran a 13): ");
+        numero = Convert.ToInt32(Console.ReadLine());
+
+        while (contador < numero)
+        {
+            contador++;
+            numSuma += contador;
+
+        }
+        Console.WriteLine($"El numero total es {numSuma}");
+        break;
+
+    case 2:
+        //Variables
+        int numero2 = 0;
+        int parells = 0;
+        int imparells = 0;
+        //Codi
+        Console.WriteLine("Introdueix un numero: ");
+        numero2 = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 1; i <= numero2; i++)
+        {
+            if (i % 2 == 0)
+            {
+                parells++;
+            }
+            else
+            {
+                imparells++;
+            }
+        }
+        Console.WriteLine($"Hi han {parells} parells i {imparells} imparells.");
+        break;
+    
+    case 3:
+        //Variables
+        int Jugada1;
+        int Jugada2;
+        string J0 = "Pedra";
+        string J1 = "Paper";
+        string J2 = "Tisores";
+        //Codi 
+        Console.Write("[+] Jugador 1 | Introdueix la teva jugada: ");
+        Jugada1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("[+] Jugador 2 | Introdueix la teva jugada: ");
+        Jugada2 = Convert.ToInt32(Console.ReadLine());
+
+        if (Jugada1 == Jugada2)
+        {
+            Console.WriteLine("EMPAT!");
+        }
+        else
+        {
+            // Jugada 1 == 0
+            if (Jugada1 == 0 && Jugada2 == 1)
+            {
+                Console.WriteLine("\n--- Jugades ---");
+                Console.WriteLine($"Jugador 1: {J0}");
+                Console.WriteLine($"Jugador 2: {J1}");
+                Console.WriteLine("[!] Guanya el jugador 2!");
+            }
+            else if (Jugada1 == 0 && Jugada2 == 2)
+            {
+                Console.WriteLine("\n--- Jugades ---");
+                Console.WriteLine($"Jugador 1: {J0}");
+                Console.WriteLine($"Jugador 2: {J2}");
+                Console.WriteLine("[!] Guanya el jugador 1!");
+            }
+            // Jugada 1 == 1
+            else if ( Jugada1 == 1 && Jugada2 == 0)
+            {
+                Console.WriteLine("\n--- Jugades ---");
+                Console.WriteLine($"Jugador 1: {J1}");
+                Console.WriteLine($"Jugador 2: {J0}");
+                Console.WriteLine("[!] Guanya el jugador 1!");
+            }
+            else if (Jugada1 == 1 && Jugada2 == 2)
+            {
+                Console.WriteLine("\n--- Jugades ---");
+                Console.WriteLine($"Jugador 1: {J1}");
+                Console.WriteLine($"Jugador 2: {J2}");
+                Console.WriteLine("[!] Guanya el jugador 2!");
+            }
+            // Jugada 1 == 2
+            else if (Jugada1 == 2 && Jugada2 == 0)
+            {
+                Console.WriteLine("\n--- Jugades ---");
+                Console.WriteLine($"Jugador 1: {J2}");
+                Console.WriteLine($"Jugador 2: {J0}");
+                Console.WriteLine("[!] Guanya el jugador 2!");
+            }
+            else if (Jugada1 == 1 && Jugada2 == 1)
+            {
+                Console.WriteLine("\n--- Jugades ---");
+                Console.WriteLine($"Jugador 1: {J2}");
+                Console.WriteLine($"Jugador 2: {J1}");
+                Console.WriteLine("[!] Guanya el jugador 1!");
+            }
+        }
+        break;
+
+    case 4:
+        // Variables
+        int accio;
+        int depositar;
+        int saldo = 1000;
+        int retirar;
+
+        //Codi
+        Console.WriteLine("Quina acció vols realitzar?\n -Introdueix 1 si vols veure el saldo\n -Introdueix 2 si vols depositar diners\n -Introdueix 3 si vols retirar diners");
+        accio = Convert.ToInt32(Console.ReadLine());
+        if (accio == 1)
+        {
+            Console.WriteLine($"aquest es el teu saldo actual: {saldo}");
+        }
+        else if (accio == 2)
+        {
+            Console.WriteLine("Quants diners vols depositar");
+            depositar = Convert.ToInt32(Console.ReadLine());
+            saldo = saldo + depositar;
+            Console.WriteLine($"Ja esta depositat, el teu saldo actual es de {saldo}");
+        }
+        else if (accio == 3)
+        {
+            Console.WriteLine("Quants diners vols retirar");
+            retirar = Convert.ToInt32(Console.ReadLine());
+            if (retirar > saldo)
+            {
+                Console.WriteLine("No pots retirar mes diners del que hi ha!");
+            }
+            else
+            {
+                saldo = saldo - retirar;
+                Console.WriteLine($"Ja esta retirat, el teu saldo actual es de {saldo}");
+            }
+        }
+        break;
+}
+
---
+++ Practica 3 - Funcions, Strings, Llistes/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq; // Average()
+using System.Security.Cryptography;
+
+//Variables Globals:
+int accio;
+int saldo = 1000;
+
+//Funcions
+void ObservarSaldo()
+{
+    Console.WriteLine($"Aquest es el teu saldo actual: {saldo}");
+}
+
+void depositarDiners(int quantitatDepositar)
+{
+    saldo += quantitatDepositar;
+    Console.WriteLine($"S'ha depositat {quantitatDepositar}$ i ara tens: {saldo}$ al teu compe.");
+}
+
+void retirarDiners(int quantitatRetirar)
+{ 
+    saldo -= quantitatRetirar;
+    Console.WriteLine($"Has retirat {quantitatRetirar}$ i ara tens {saldo}$ al teu compte.");
+}
+
+//
+int exercici = 0;
+
+Console.WriteLine("Quin exercici vols triar? ");
+exercici = Convert.ToInt32(Console.ReadLine());
+
+switch (exercici)
+{
+    case 1:
+        //Variables
+        List<int> ListaNumeros = new List<int>();
+            
+        //Codigo
+        for (int i = 0; i < 55; i++)
+        {
+            int numeroRandom = Random.Shared.Next(22, 1559);
+            ListaNumeros.Add(numeroRandom);
+        }     
+        int minim = ListaNumeros[0];
+        int maxim = ListaNumeros[0];
+       
+
+        foreach (int num in ListaNumeros)
+        {
+            if (minim > num)
+                minim = num;
+            if (maxim < num)
+                maxim = num;
+        }
+        Console.WriteLine($"La llista de numeros generada es: {string.Join(", ", ListaNumeros)}\n");
+        Console.WriteLine($"El numero mes petit es: {minim}");
+        Console.WriteLine($"El numero mes gran es: {maxim}");
+    break;
+
+    case 2:
+        //Variables
+        List<int> ListaNumeros333 = new List<int>();
+        int count4 = 0;
+        int mesQueMitjana = 0;
+        //Codi
+        for (int i = 0; i < 333; i++)
+        {
+            int numeroRandom2 = new Random().Next();
+            ListaNumeros333.Add(numeroRandom2);
+        }
+
+        //Mitjana
+        double mitjana = ListaNumeros333.Average();
+        Console.WriteLine($"La mitjana dels 333 numeros es: {mitjana}");
+
+        //Multiples de 4
+        foreach (int i in ListaNumeros333)
+        {
+            if (i % 4 == 0)
+            count4++;
+        }
+        Console.WriteLine($"Hi ha {count4} multiples de 4");
+
+        //Mes gran que la mitjana:
+        foreach (int o in ListaNumeros333)
+        {
+            if (o > mitjana)
+                mesQueMitjana++;
+        }
+        Console.WriteLine($"Hi ha {mesQueMitjana} numeros mes grans que la mitjana.");
+    break;
+
+    case 3:
+        //Variables & Listas
+        string string3;
+        int vocalsCount = 0;
+        string Lstring3;
+        //Codi
+        Console.Write("Introdueix una frase per analitzar: ");
+        string3 = Convert.ToString(Console.ReadLine());
+        Lstring3 = string3.ToLower();
+
+        foreach (char u in Lstring3)
+        {
+            if (u == 'a' || u == 'e' || u == 'i' || u == 'o' || u == 'u')
+                vocalsCount++;
+        }
+        Console.WriteLine($"Hi ha {vocalsCount} vocals a la teva frase.");
+    break;
+
+    case 4:
+        //Variables
+        int quantitatDepositarEX;
+        int quantitatRetirarEX;
+        
+        //Codi
+        Console.WriteLine("Quina acció vols realitzar?\n - Introdueix 1 si vols veure el teu saldo.\n - Introdueix 2 si vols depositar diners.\n - Introdueix 3 si vols retirar diners.");
+        accio = Convert.ToInt32(Console.ReadLine());
+
+        switch (accio)
+        {
+            case 1:
+                ObservarSaldo();
+                break;
+
+            case 2:
+                Console.Write("Quant saldo vols depositar? ");
+                quantitatDepositarEX = Convert.ToInt32(Console.ReadLine());
+                depositarDiners(quantitatDepositarEX);
+                break;
+
+            case 3:
+                Console.Write("Quant saldo vols retirar? ");
+                quantitatRetirarEX = Convert.ToInt32(Console.ReadLine());
+                retirarDiners(quantitatRetirarEX);
+                break;
+        }
+        break;
+}
+
